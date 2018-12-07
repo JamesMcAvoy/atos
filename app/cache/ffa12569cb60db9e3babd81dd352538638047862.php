8e4d2ca0f70c5d833ae849a08dda489d6339b5ca
JamesMcAvoy <mathieu.brisset@laposte.net>
2018-12-07 01:58:15 +0100

    index

---
+++ app/cache/ffa12569cb60db9e3babd81dd352538638047862.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="container mt-5 mb-5">
+        <?php echo $__env->yieldContent('main_content'); ?>
+    </div>
+
+</body>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -1,1 +1,12 @@
 <?php
+
+require '../vendor/autoload.php';
+
+use Philo\Blade\Blade;
+use Goutte\Client;
+
+$views = __DIR__ . '/../app/views';
+$cache = __DIR__ . '/../app/cache';
+
+$blade = new Blade($views, $cache);
+echo $blade->view()->make('index')->render();
